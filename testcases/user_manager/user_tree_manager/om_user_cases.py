8689617e54767139f455a3b7ab02f49497c59c95
liuzhongyi <18208670421@163.com>
2020-07-30 16:49:28 +0800

    添加安装文件

--- testcases/user_manager/user_tree_manager/om_user_cases.py
+++ testcases/user_manager/user_tree_manager/om_user_cases.py
@@ -25,11 +25,7 @@ class user_manager(unittest.TestCase):
     def test_findallMenber(self):
         """[case user01]获取所有成员信息"""
         logger.info('获取om的所有用户信息')
-        headers = {
-             'Authorization': 'Bearer ' + OM_login.access_token['access_token'],
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-            'Accept': 'application/json, text/plain, */*'
-        }
+
         params = {
             'size': 10,
             'current': 1,
@@ -42,11 +38,6 @@ class user_manager(unittest.TestCase):
     def test_getRole(self):
         """[case user01]获取所有角色信息"""
         logger.info('获取om的所有角色信息')
-        headers = {
-            'Authorization': 'Bearer ' + OM_login.access_token['access_token'],
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-            'Accept': 'application/json, text/plain, */*'
-        }
         params = {
             'size': 10,
             'current': 1,
@@ -57,11 +48,6 @@ class user_manager(unittest.TestCase):
     def test_greatemember(self):
         """[case user01]创建用户"""
         logger.info('创建用户')
-        headers = {
-            'Authorization': 'Bearer ' + OM_login.access_token['access_token'],
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-            'Accept': 'application/json, text/plain, */*'
-        }
         params = {"trelloName":"","username":"fff","sex":"0","role":[2],"phone":"15552115654","email":"23@qq.com","password":"123456","birthDate":"2020-07-22","staffNumber":"","deptId":1,"deptName":"","lockFlag":"0","$lockFlag":"有效"}
 
         respose= self.session.get(url=self.hosts+'/apis/admin/user',headers=self.headers,data=params)
@@ -69,11 +55,6 @@ class user_manager(unittest.TestCase):
 
     def test_tree(self):
         """[]查询所有的菜单权限管理"""
-        headers = {
-            'Authorization': 'Bearer ' + OM_login.access_token['access_token'],
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-            'Accept': 'application/json, text/plain, */*'
-        }
         respose=self.session.get(url=self.hosts+'/apis/admin/menu/tree',headers=self.headers)
         print(respose.text)
 
